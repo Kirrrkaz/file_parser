966834dc775fc59dc7dcb44abaa056e05a645a33
Kirrrkaz <74609707+Kirrrkaz@users.noreply.github.com>
2021-08-10 20:25:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@
 from bs4 import BeautifulSoup
 with open("index.html", encoding='utf-8') as file:
     src = file.read()
-
+#print(src)
 
 soup = BeautifulSoup(src, 'html.parser')
 
-title = soup.title
-print(title.text)
+#title = soup.title
+#print(title.text)
 
+links = soup.find_all("li")
+print(links)
 
 
