10c7c98f39e679e5b7d2a9352224c5331957a9f2
Kirrrkaz <74609707+Kirrrkaz@users.noreply.github.com>
2021-08-10 20:14:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+#pip install beautifulsoup4
+#pip install lxml
 
+from bs4 import BeautifulSoup
 with open("index.html", encoding='utf-8') as file:
     src = file.read()
-print(src)
 
+
+soup = BeautifulSoup(src, 'html.parser')
+
+title = soup.title
+print(title.text)
 
 
 
